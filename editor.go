cf40cf6667ed9d6be06570a60071cd07e2db8608
AlejandroSuero <asuerome@gmail.com>
2024-01-16 15:18:30 +0100

    Implemented scroll and navigation
    Fixed a bug that scrolled in the opossite direction, modified goedit command alone to display a welcome page.\n\nThe navigation keeps the cursor in the previous column if same lenght and there was no changes in the column via a keypress

--- editor.go
+++ editor.go
@@ -16,6 +16,7 @@ var mode int
 var ROWS, COLS int
 var offset_row, offset_col int
 var current_row, current_col int
+var prev_row, prev_col int
 
 var source_file string
 
@@ -23,6 +24,8 @@ var text_buffer = [][]rune{}
 var undo_buffer = [][]rune{}
 var copy_buffer = []rune{}
 var modified bool
+var cursor_changed bool
+var is_welcome_page bool
 
 func readFile(filename string) {
 	file, err := os.Open(filename)
@@ -46,6 +49,7 @@ func readFile(filename string) {
 	if line_number == 0 {
 		text_buffer = append(text_buffer, []rune{})
 	}
+	is_welcome_page = false
 }
 
 func printMessage(col int, row int, fg termbox.Attribute, bg termbox.Attribute, message string) {
@@ -59,22 +63,101 @@ func displayLineNumber(line_number int) {
 	termbox.SetCell(0, line_number, rune(strconv.Itoa(line_number + 1)[0]), termbox.ColorBlack, termbox.ColorDefault)
 }
 
+func scrollTextBuffer() {
+	if current_row < offset_row {
+		offset_row = current_row
+	}
+	if current_col < offset_col {
+		offset_col = current_col
+	}
+	if current_row >= offset_row+ROWS {
+		offset_row = current_row - ROWS + 1
+	}
+	if current_col >= offset_col+COLS {
+		offset_col = current_col - COLS + 1
+	}
+}
+
+/*
+Moves the cursor to the desired direction
+
+direction can be = "up" | "down" | "left" | "right" | "home" | "end" | "pageUp" | "pageDown"
+*/
+func moveCursor(direction string) {
+	if cursor_changed {
+		prev_col = current_col
+	}
+	switch direction {
+	case "up":
+		cursor_changed = false
+		current_col = prev_col
+		if current_row != 0 {
+			current_row--
+		}
+	case "down":
+		cursor_changed = false
+		current_col = prev_col
+		if current_row < len(text_buffer)-1 {
+			current_row++
+		}
+	case "left":
+		cursor_changed = true
+		if current_col != 0 {
+			current_col--
+		} else if current_row > 0 {
+			current_row--
+			current_col = len(text_buffer[current_row])
+		}
+
+	case "right":
+		cursor_changed = true
+		if current_col < len(text_buffer[current_row]) {
+			current_col++
+		} else if current_row < len(text_buffer)-1 {
+			current_row++
+			current_col = 0
+		}
+	case "home":
+		cursor_changed = true
+		current_col = 0
+	case "end":
+		cursor_changed = true
+		current_col = len(text_buffer[current_row])
+	case "pageUp":
+		cursor_changed = true
+		if current_row-int(ROWS/2) > 0 {
+			current_row -= int(ROWS / 2)
+		} else {
+			current_row = 0
+		}
+	case "pageDown":
+		cursor_changed = true
+		if current_row+int(ROWS/2) < len(text_buffer)-1 {
+			current_row += int(ROWS / 2)
+		} else {
+			current_row = len(text_buffer) - 1
+		}
+	default:
+		termbox.Close()
+		panic("\tThe direction: \"" + direction + "\" is not a defined direction\n\tValid directions: \"up\", \"down\", \"left\", \"right\", \"left\", \"home\", \"end\", \"pageUp\" or \"pageDown\"\n\tPlease check your code.")
+	}
+}
+
 func displayTextBuffer() {
 	var row, col int
 	for row = 0; row < ROWS; row++ {
-		text_buffer_row := row + offset_col
+		text_buffer_row := row + offset_row
 		for col = 0; col < COLS; col++ {
-			text_buffer_col := col + offset_row
+			text_buffer_col := col + offset_col
 			if text_buffer_row >= 0 && text_buffer_row < len(text_buffer) && text_buffer_col < len(text_buffer[text_buffer_row]) {
 				if text_buffer[text_buffer_row][text_buffer_col] != '\t' {
-					termbox.SetChar(col+2, row, text_buffer[text_buffer_row][text_buffer_col])
+					termbox.SetChar(col, row, text_buffer[text_buffer_row][text_buffer_col])
 				} else {
-					termbox.SetCell(col+2, row, rune(' '), termbox.ColorDefault, termbox.ColorGreen)
+					termbox.SetCell(col, row, rune(' '), termbox.ColorDefault, termbox.ColorGreen)
 				}
+			} else if row+offset_row > len(text_buffer)-1 {
+				termbox.SetCell(0, row, rune('~'), termbox.ColorBlue, termbox.ColorDefault)
 			}
-		}
-		if row+offset_row > len(text_buffer) {
-			termbox.SetCell(0, row, rune('~'), termbox.ColorBlue, termbox.ColorDefault)
 		}
 		termbox.SetChar(col, row, rune('\n'))
 	}
@@ -113,7 +196,61 @@ func displayStatusBar() {
 	used_space := len(mode_status) + len(file_status) + len(cursor_status) + len(copy_status) + len(undo_status)
 	spaces := strings.Repeat(" ", COLS-used_space)
 	message := mode_status + file_status + copy_status + undo_status + spaces + cursor_status
-	printMessage(0, ROWS, termbox.ColorBlack, termbox.ColorCyan, message)
+	printMessage(0, ROWS, termbox.ColorBlack, termbox.ColorBlue, message)
+}
+
+func getKey() termbox.Event {
+	var key_event termbox.Event
+	switch event := termbox.PollEvent(); event.Type {
+	case termbox.EventKey:
+		key_event = event
+	case termbox.EventError:
+		panic(event.Err)
+	}
+	return key_event
+}
+
+func processKeyPress() {
+	key_event := getKey()
+	key := key_event.Key
+	ch := key_event.Ch
+	if key == termbox.KeyEsc || key == termbox.KeyCtrlQ {
+		termbox.Close()
+		os.Exit(0)
+	} else if ch != 0 {
+		// Character handler
+	} else {
+		switch key {
+		case termbox.KeyHome:
+			moveCursor("home")
+		case termbox.KeyEnd:
+			moveCursor("end")
+		case termbox.KeyPgup:
+			moveCursor("pageUp")
+		case termbox.KeyPgdn:
+			moveCursor("pageDown")
+		case termbox.KeyArrowDown:
+			moveCursor("down")
+		case termbox.KeyArrowUp:
+			moveCursor("up")
+		case termbox.KeyArrowLeft:
+			moveCursor("left")
+		case termbox.KeyArrowRight:
+			moveCursor("right")
+		}
+		if current_col > len(text_buffer[current_row]) {
+			current_col = len(text_buffer[current_row])
+		}
+	}
+}
+
+func displayWelcomePage(display bool) {
+	if display {
+		message := "GoEdit - A minimalistic text editor written in GoLang"
+		printMessage(int((COLS-len(message))/2), int(ROWS/2)-1, termbox.ColorBlue, termbox.ColorDefault, message)
+		message = "What are you waiting for? GoEdit those files :)"
+		printMessage(int((COLS-len(message))/2), int(ROWS/2)+1, termbox.ColorBlue, termbox.ColorDefault, message)
+	}
 }
 
 func goEdit() {
@@ -133,22 +270,21 @@ func goEdit() {
 		}
 		readFile(source_file)
 	} else {
-		source_file = "out.txt"
+		source_file = "welcome.txt"
 		text_buffer = append(text_buffer, []rune{})
+		is_welcome_page = true
 	}
-
 	for {
 		COLS, ROWS = termbox.Size()
 		ROWS--
 		termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
+		scrollTextBuffer()
 		displayTextBuffer()
+		displayWelcomePage(is_welcome_page)
 		displayStatusBar()
+		termbox.SetCursor(current_col-offset_col, current_row-offset_row)
 		termbox.Flush()
-		event := termbox.PollEvent()
-		if event.Type == termbox.EventKey && (event.Key == termbox.KeyEsc || event.Key == termbox.KeyCtrlQ) {
-			termbox.Close()
-			break
-		}
+		processKeyPress()
 	}
 }
 
